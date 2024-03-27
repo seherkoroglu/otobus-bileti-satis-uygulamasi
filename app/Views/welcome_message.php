553a2271debad7b49f2c6be7d87195ed38518633
betül <211307033@uzem.education>
2024-03-27 12:56:11 +0300

    Giriş/Üye link verme

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -21,10 +21,10 @@
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Giriş Yap</a>
+          <a class="nav-link active" aria-current="page" href="<?= site_url('/login'); ?>">Giriş Yap</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Üye Ol</a>
+          <a class="nav-link" href="<?= site_url('/register'); ?>">Üye Ol</a>
         </li>
        
       </ul>
